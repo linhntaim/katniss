fa12a3cd575531d5e3108a667423166cdef6264b
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2016-12-06 14:02:39 +0700

    Appy Request extends

--- public/index.php
+++ public/index.php
@@ -50,6 +50,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
+    $request = Katniss\Everdeen\Http\Request::capture() // custom request class
 );
 
 $response->send();
