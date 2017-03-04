567e2423a11d9f5ec05543992fcfc4ef221fb092
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2017-03-05 00:29:14 +0700

    Fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,39 +11,39 @@ const { mix } = require('laravel-mix');
  |
  */
 
-// mix.combine([
-//     'resources/assets/js/js.cookie.js',
-//     'resources/assets/js/helpers.js',
-//     'resources/assets/js/class.number_format_helper.js',
-//     'resources/assets/js/class.katniss_api.js',
-//     'resources/assets/js/refresh_session.js',
-//     'resources/assets/js/gui.jquery.js'
-// ], 'public/assets/libraries/katniss.home.js');
-//
-// mix.combine([
-//     'resources/assets/js/js.cookie.js',
-//     'resources/assets/js/helpers.js',
-//     'resources/assets/js/slug.js',
-//     'resources/assets/js/slug.jquery.js',
-//     'resources/assets/js/class.number_format_helper.js',
-//     'resources/assets/js/class.katniss_api.js',
-//     'resources/assets/js/refresh_session.js',
-//     'resources/assets/js/gui.jquery.js'
-// ], 'public/assets/libraries/katniss.admin.js');
+mix.combine([
+    'resources/assets/js/js.cookie.js',
+    'resources/assets/js/helpers.js',
+    'resources/assets/js/class.number_format_helper.js',
+    'resources/assets/js/class.katniss_api.js',
+    'resources/assets/js/refresh_session.js',
+    'resources/assets/js/gui.jquery.js'
+], 'public/assets/libraries/katniss.home.js');
 
-// mix.combine([
-//     'resources/assets/js/realtime.pusher.js',
-//     'resources/assets/js/sounds.js',
-//     'resources/assets/js/conversation.js'
-// ], 'public/assets/libraries/katniss.conversation.js');
-// mix.styles(['resources/assets/css/conversation.css'], 'public/assets/libraries/katniss.conversation.css');
+mix.combine([
+    'resources/assets/js/js.cookie.js',
+    'resources/assets/js/helpers.js',
+    'resources/assets/js/slug.js',
+    'resources/assets/js/slug.jquery.js',
+    'resources/assets/js/class.number_format_helper.js',
+    'resources/assets/js/class.katniss_api.js',
+    'resources/assets/js/refresh_session.js',
+    'resources/assets/js/gui.jquery.js'
+], 'public/assets/libraries/katniss.admin.js');
 
-// mix.combine(['resources/assets/css/modal_cropper_image.css'], 'public/assets/libraries/modal_cropper_image.css');
-// mix.scripts(['resources/assets/js/modal_cropper_image.js'], 'public/assets/libraries/modal_cropper_image.js');
-//
-// mix.combine([
-//     'resources/assets/js/google_maps_markers.js'
-// ], 'public/assets/libraries/google_maps_markers.js');
+mix.combine([
+    'resources/assets/js/realtime.pusher.js',
+    'resources/assets/js/sounds.js',
+    'resources/assets/js/conversation.js'
+], 'public/assets/libraries/katniss.conversation.js');
+mix.styles(['resources/assets/css/conversation.css'], 'public/assets/libraries/katniss.conversation.css');
+
+mix.combine(['resources/assets/css/modal_cropper_image.css'], 'public/assets/libraries/modal_cropper_image.css');
+mix.scripts(['resources/assets/js/modal_cropper_image.js'], 'public/assets/libraries/modal_cropper_image.js');
+
+mix.combine([
+    'resources/assets/js/google_maps_markers.js'
+], 'public/assets/libraries/google_maps_markers.js');
 
 mix.combine([
     'resources/assets/css/wow_skype/common.css',
