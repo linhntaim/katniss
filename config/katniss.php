408ff460fdbb1fe879482c9658e5fb6940a54305
Nguyen Tuan Linh <linhnt.aim@outlook.com>
2017-04-21 01:34:48 +0700

    fix

--- config/katniss.php
+++ config/katniss.php
@@ -7,6 +7,17 @@
  */
 
 return [
+    'app' => [
+        'description' => env('APP_DESCRIPTION', ''),
+        'keywords' => env('APP_KEYWORDS', ''),
+        'short_name' => env('APP_SHORT_NAME', ''),
+        'version' => env('APP_VERSION', ''),
+        'author' => env('APP_AUTHOR', ''),
+        'email' => env('APP_EMAIL', ''),
+        'logo' => env('APP_LOGO', ''),
+        'default_user_picture' => env('APP_DEFAULT_USER_PICTURE', ''),
+    ],
+
     'settings' => [
         'city' => 'LA',
         'country' => 'US',
